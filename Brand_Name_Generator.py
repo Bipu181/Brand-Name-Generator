b0922fe74df7894d03d77b66ef7e2d17e6e58d38
Bipu181 <bipupatowari7@gmail.com>
2024-07-11 15:42:16 +0600

    Add files via upload

---
+++ Brand_Name_Generator.py
@@ -0,0 +1,7 @@
+print("Welcome to the Brand Name Generator")
+city=input("What is the Name of the city you Grew up in?\n")
+c=city.capitalize()
+pet=input("What is your pet name?\n")
+p=pet.capitalize()
+print("Your Brand name could be",c,p)
+
